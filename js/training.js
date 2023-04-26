af26884150ec6b30cec707fd5727d058fdb9c064
Rowan Byzantium <verte735@gmail.com>
2023-04-26 16:58:39 +0200

    ex 1 & 2

--- js/training.js
+++ js/training.js
@@ -1,14 +1,40 @@
 /* ------------------------------------ */
 /* --- Exercice 1 --- */
 
+const button = document.querySelector("#ex1 .button");
+console.log(button);
+
+button.addEventListener('click', function (event) {
+    //remove enlève sans possibilité de remettre
+    // button.parentElement.removeChild(p1);
+    document.getElementById('p1').classList.toggle("display-hidden")
+});
 
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
 
+// ex2-scroll-value = scrollY
+const scrollvalue = document.querySelector('#ex2-scroll-value')
+console.log(scroll);
+
+window.addEventListener('scroll', function (event) {
+    scrollvalue.textContent = Math.round(window.scrollY)
+})
 
 /* ------------------------------------ */
 /* --- Exercice 3 --- */
 
+const animal = document.querySelector('#ex3-animals');
+console.log(animal);
+
+const lastAnimal = animal.lastElementChild;
+console.log(lastAnimal);
+
+button = document.querySelector('#ex3 .button')
+
+button.addEventListener('click', function(event){
+    animal.appendChild(lastAnimal)
+})
 
 /* ------------------------------------ */
 /* --- Exercice 4 --- */
